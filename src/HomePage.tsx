6c791680aae3bde1672b41669019960aa91eea1d
Blake <80996688+BlakeTnr@users.noreply.github.com>
2025-02-23 14:37:42 -0500

    Layout homepage and about
    Finished up the layouting for the homepage, adding assets, and layouting the about section. Hompeage is done.

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,64 +1,89 @@
 import React from 'react'
 import LayoutDefault from './LayoutDefault'
-import { Box, Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material'
+import { Box, Button, Card, CardActionArea, CardContent, CardMedia, Grid, Grid2, Typography } from '@mui/material'
+import ReportLostItem from '../public/report-lost-item.png'
+import ReportFoundItem from '../public/report-found-item.png'
+import ClaimItem from '../public/claim-item.png'
 
 function HomePage() {
   return (
     <>
         <LayoutDefault>
-            <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-            <Typography variant="h4" gutterBottom>
-            Lost something? Found something?
-            </Typography>
-            <Typography variant="h2" gutterBottom>
-            Lost and Found Portal
-            </Typography>
-            </Box>
+                <Box sx={{ display: 'flex', justifyContent: 'right', gap: '20px', margin: '20px' }}>
+                    <Button variant="contained">Login</Button>
+                    <Button variant="contained">Register</Button>
+                </Box>
+                
+                <Box sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+                    <Typography variant="h4" gutterBottom>
+                    Lost something? Found something?
+                    </Typography>
+                    <Typography variant="h2" gutterBottom>
+                    Lost and Found Portal
+                    </Typography>
+                </Box>
 
-            <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-                <Card sx={{ maxWidth: 345 }}>
-                    <CardActionArea>
-                        <CardMedia
-                        component="img"
-                        image="/report-lost-item.png"
-                        alt="green iguana"
-                        />
-                        <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            Report Lost Item
+                <Grid container justifyContent={'center'} spacing={12}>
+                    <Grid item size={4}>
+                        <Card sx={{ maxWidth: 345 }}>
+                            <CardActionArea>
+                                <CardMedia
+                                component="img"
+                                image={ReportLostItem}
+                                alt="green iguana"
+                                />
+                                <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Report Lost Item
+                                </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
+                    <Grid item size={4}>
+                        <Card sx={{ maxWidth: 345 }}>
+                            <CardActionArea>
+                                <CardMedia
+                                component="img"
+                                image={ReportFoundItem}
+                                alt="green iguana"
+                                />
+                                <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Report Found Item
+                                </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
+                    <Grid item size={4}>
+                        <Card sx={{ maxWidth: 345 }}>
+                            <CardActionArea>
+                                <CardMedia
+                                component="img"
+                                image={ClaimItem}
+                                alt="green iguana"
+                                />
+                                <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Claim Item
+                                </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
+                </Grid>
+
+                <Grid2 container columns={{ xs: 4, sm: 8, md: 12 }} marginTop={'auto'} backgroundColor={'black'} padding={'15px'}>
+                    <Grid2 item size={4}>
+                        <Typography variant="h4" gutterBottom>
+                        About
                         </Typography>
-                        </CardContent>
-                    </CardActionArea>
-                </Card>
-                <Card sx={{ maxWidth: 345 }}>
-                    <CardActionArea>
-                        <CardMedia
-                        component="img"
-                        image="/report-lost-item.png"
-                        alt="green iguana"
-                        />
-                        <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            Report Found Item
+                        <Typography variant="body" gutterBottom>
+                        We connect people with their lost belongings through a simple and efficient platform. Report lost or found items, browse listings, and get reunited with what matters.
                         </Typography>
-                        </CardContent>
-                    </CardActionArea>
-                </Card>
-                <Card sx={{ maxWidth: 345 }}>
-                    <CardActionArea>
-                        <CardMedia
-                        component="img"
-                        image="/report-lost-item.png"
-                        alt="green iguana"
-                        />
-                        <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            Claim Item
-                        </Typography>
-                        </CardContent>
-                    </CardActionArea>
-                </Card>
-            </Box>
+                    </Grid2>
+                </Grid2>
         </LayoutDefault>
     </>
   )
--- src/LayoutDefault.tsx
+++ src/LayoutDefault.tsx
@@ -1,11 +1,14 @@
 import React, { Fragment } from 'react';
 import { Route } from 'react-router';
 import ResponsiveAppBar from './components/ResponsiveAppBar';
+import { Box } from '@mui/material';
 
 const LayoutDefault = (props: any) => (
   <>
-    <ResponsiveAppBar />
-    {props.children}
+    <Box sx={{minHeight: '100vh', display: 'flex', flexDirection: 'column'}}>
+      <ResponsiveAppBar />
+      {props.children}
+    </Box>
   </>
 );
 
