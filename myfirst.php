9ebbc690a2d0904230487e3a8f1e2008efa69ace
jxboulwa <jxboulwa@gmail.com>
2025-02-27 13:44:31 -0500

    added database name

--- myfirst.php
+++ myfirst.php
@@ -1,11 +1,12 @@
 <?php
-$servername = "localhost";  // Connect through the SSH tunnel
-$port = 3306;               // Use the local forwarded port
+$servername = "localhost";   
+$port = 3306;                
 $username = "jxboulwa";
 $password = "50456062";
+$database = "cse442_2025_spring_team_s_db";
 
 try {
-    $conn = new PDO("mysql:host=$servername;port=$port;charset=utf8", $username, $password);
+    $conn = new PDO("mysql:host=$servername;port=$port;dbname=$database;charset=utf8", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "✅ Connected successfully!";
 } catch (PDOException $e) {
