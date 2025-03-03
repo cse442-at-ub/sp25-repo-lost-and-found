76da1f3c2b0eb2a20b44f473837f8aa31eb44e21
Blake <80996688+BlakeTnr@users.noreply.github.com>
2025-03-03 16:39:19 -0500

    Bug fixes
    This fixes some of the bugs that occured where when deployed to prod there was a 2 pathing issues. This made it so the page wouldn't load and now it loads for the end users.

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CSE442/2025-Spring/cse-442s/dinal/dist/',
+  base: '/CSE442/2025-Spring/cse-442s/',
 })
