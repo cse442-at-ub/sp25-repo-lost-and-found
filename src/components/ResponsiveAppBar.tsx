be2d1f31e3c258042bb9f115857d82b670797998
Blake <80996688+BlakeTnr@users.noreply.github.com>
2025-03-03 21:29:28 -0500

    Logging out
    Made it so logging out deletes all cookies on the users side so that they can effective logout
    
    Also cleaned up some console debug logging

--- src/components/ResponsiveAppBar.tsx
+++ src/components/ResponsiveAppBar.tsx
@@ -46,13 +46,19 @@ function ResponsiveAppBar() {
       return
     }
 
-    console.log(pageName)
-
     navigate("/" + pageName)
   }
 
+  function deleteAllCookies() {
+    document.cookie.split(';').forEach(cookie => {
+        const eqPos = cookie.indexOf('=');
+        const name = eqPos > -1 ? cookie.substring(0, eqPos) : cookie;
+        document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
+    });
+  }
+
   const Logout = () => {
-    document.cookie.split(";").forEach(function(c) { document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/"); });
+    deleteAllCookies()
     handleCloseUserMenu()
   }
 
