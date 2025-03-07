fbe189693c2470c1322480129487e099a1fba8a7
jxboulwa <jxboulwa@gmail.com>
2025-03-07 08:17:29 -0500

    cattle did not update these two file
    dont know why cattle put these two file to different folder,

--- Backend/src/resetPassword.php
+++ Backend/src/resetPassword.php
@@ -1,9 +1,11 @@
 <?php
 // Database configuration
 $host = 'localhost'; // e.g., localhost
+$host = 'localhost'; // e.g., localhost
 $dbname = 'cse442_2025_spring_team_s_db';
 $username = 'jxboulwa';
 $password = '50456062';
+
 
 try {
   // Establish PDO connection
@@ -25,12 +27,12 @@ try {
       return;
     }
 
-    if (isset($data['otp'])) {
-      $otp = $data['otp'];
-    } else {
-      echo '{"okay": false, "msg": "OTP Token required"}';
-      return;
-    }
+    // if (isset($data['otp'])) {
+    //   $otp = $data['otp'];
+    // } else {
+    //   echo '{"okay": false, "msg": "OTP Token required"}';
+    //   return;
+    // }
 
     if (isset($data['password'])) {
       $password = $data['password'];
@@ -53,16 +55,16 @@ try {
 
     $stmt = $pdo->prepare('SELECT id, token FROM otp_tokens
         WHERE email = :email and used = FALSE
-        and created_at >= DATE_SUB(NOW(), INTERVAL 2 MINUTE)');
+        and created_at >= DATE_SUB(NOW(), INTERVAL 10 MINUTE)');
     $stmt->bindValue(':email', $email, PDO::PARAM_STR);
     $stmt->execute();
 
-    while (false !== ($row = $stmt->fetch())) {
-      if (password_verify($otp, $row['token'])) {
-        $stmt = $pdo->prepare('UPDATE otp_tokens SET used = TRUE
-            WHERE id = :id');
-        $stmt->bindValue(':id', $row['id'], PDO::PARAM_INT);
-        $stmt->execute();
+    // while (false !== ($row = $stmt->fetch())) {
+    //   if (password_verify($otp, $row['token'])) {
+    //     $stmt = $pdo->prepare('UPDATE otp_tokens SET used = TRUE
+    //         WHERE id = :id');
+    //     $stmt->bindValue(':id', $row['id'], PDO::PARAM_INT);
+    //     $stmt->execute();
 
         $stmt = $pdo->prepare('UPDATE users SET password = :password
             WHERE email = :email');
@@ -71,14 +73,14 @@ try {
         $stmt->execute();
 
         if ($stmt->rowCount() > 0) {
-          echo '{"okay": true, "msg": "Password updated"}';
+          echo '{"okay": true, "msg": "Password updated, an admin will email you with detail."}';
         } else {
           echo '{"okay": false, "msg": "Failed to update password"}';
         }
-        return;
-      }
-    }
-    echo '{"okay": false, "msg": "Invalid email or OTP token"}';
+    //     return;
+    //   }
+    // }
+    // echo '{"okay": false, "msg": "Invalid email or OTP token"}';
   }
 } catch (PDOException $e) {
   // Handle database errors
--- Backend/src/sendOtp.php
+++ Backend/src/sendOtp.php
@@ -7,10 +7,12 @@ require 'PHPMailer-6.9.3/src/PHPMailer.php';
 require 'PHPMailer-6.9.3/src/SMTP.php';
 
 // Database configuration
+
 $host = 'localhost'; // e.g., localhost
 $dbname = 'cse442_2025_spring_team_s_db';
 $username = 'jxboulwa';
 $password = '50456062';
+
 
 try {
   // Establish PDO connection
@@ -35,52 +37,53 @@ try {
       $count = $stmt->fetchColumn();
 
       if ($count > 0) {
-        $otp = sprintf("%06d", rand(0, 999999));
-        $hashed = password_hash($otp, PASSWORD_BCRYPT);
-        $stmt = $pdo->prepare('INSERT INTO otp_tokens
-            VALUES (DEFAULT,:email,:token,DEFAULT,DEFAULT)');
-        $stmt->bindValue(':email', $email, PDO::PARAM_STR);
-        $stmt->bindValue(':token', $hashed, PDO::PARAM_STR);
-        $stmt->execute();
+        // $otp = sprintf("%06d", rand(0, 999999));
+        // $hashed = password_hash($otp, PASSWORD_BCRYPT);
+        // $stmt = $pdo->prepare('INSERT INTO otp_tokens
+        //     VALUES (DEFAULT,:email,:token,DEFAULT,DEFAULT)');
+        // $stmt->bindValue(':email', $email, PDO::PARAM_STR);
+        // $stmt->bindValue(':token', $hashed, PDO::PARAM_STR);
+        // $stmt->execute();
 
-        $mail = new PHPMailer(true);
-        try {
-          //Server settings
-          $mail->SMTPDebug = 0; // Enable verbose debug output, change to DEBUG_OFF in production
-          $mail->isSMTP();                                            // Send using SMTP
-          $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-          $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-          $mail->SMTPSecure = 'tls';            // Enable implicit TLS encryption
-          $mail->Username   = 'cse442s@gmail.com';                     // SMTP username
-          $mail->Password   = 'ecqctoshpanmvpst';                               // SMTP password, use app password for better security
-          $mail->Port       = 587;                                    // TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        // $mail = new PHPMailer(true);
+        // try {
+        //   //Server settings
+        //   $mail->SMTPDebug = 0; // Enable verbose debug output, change to DEBUG_OFF in production
+        //   $mail->isSMTP();                                            // Send using SMTP
+        //   $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+        //   $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+        //   $mail->SMTPSecure = 'tls';            // Enable implicit TLS encryption
+        //   $mail->Username   = 'cse442s@gmail.com';                     // SMTP username
+        //   $mail->Password   = 'ecqctoshpanmvpst';                               // SMTP password, use app password for better security
+        //   $mail->Port       = 587;                                    // TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-          //Recipients
-          $mail->setFrom('voiceoverair@gmail.com', 'CSE442 Lost and Found');
-          $mail->addAddress($email, 'User');     // Add a recipient
-          // $mail->addAddress('ellen@example.com');               // Name is optional
-          // $mail->addReplyTo('info@example.com', 'Information');
-          // $mail->addCC('cc@example.com');
-          // $mail->addBCC('bcc@example.com');
+        //   //Recipients
+        //   $mail->setFrom('cse442s@gmail.com', 'CSE442 Lost and Found');
+        //   $mail->addAddress($email, 'User');     // Add a recipient
+        //   // $mail->addAddress('ellen@example.com');               // Name is optional
+        //   // $mail->addReplyTo('info@example.com', 'Information');
+        //   // $mail->addCC('cc@example.com');
+        //   // $mail->addBCC('bcc@example.com');
 
-          // Attachments
-          // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-          // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+        //   // Attachments
+        //   // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+        //   // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 
-          // Content
-          $mail->isHTML(true);                                  // Set email format to HTML
-          $mail->Subject = 'Your OTP token to reset password';
-          $mail->Body    = "<p>Your OTP token is <pre>$otp</pre>. It will expire after 10 minutes.</p>";
-          $mail->AltBody = "Your OTP token is $otp. It will expire after 10 minutes.</p>";
+        //   // Content
+        //   $mail->isHTML(true);                                  // Set email format to HTML
+        //   $mail->Subject = 'Your OTP token to reset password';
+        //   $mail->Body    = "<p>Your OTP token is <pre>$otp</pre>. It will expire after 10 minutes.</p>";
+        //   $mail->AltBody = "Your OTP token is $otp. It will expire after 10 minutes.</p>";
 
-          $mail->send();
-          echo sprintf('{"okay": true, "otp": "%s"}', $otp);
-        } catch (Exception $e) {
-          echo sprintf('{"okay": true, "otp": "%s", "msg": "%s"}', $otp,
-            "Email could not be sent. Mailer Error: {$mail->ErrorInfo}");
-        }
+        //   $mail->send();
+        //   echo sprintf('{"okay": true, "otp": "%s"}', $otp);
+        // } catch (Exception $e) {
+        //   echo sprintf('{"okay": true, "otp": "%s", "msg": "%s"}', $otp,
+        //     "Email could not be sent. Mailer Error: {$mail->ErrorInfo}");
+        // }
 
         // echo sprintf('{"okay": true, "otp": "%s"}', $otp);
+        echo sprintf('{"okay": true}', $otp);
       } else {
         echo '{"okay": false, "msg": "Email not found"}';
       }
