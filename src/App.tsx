35d463a99e60e76f7045c38ae648c50aeece16d1
Blake <80996688+BlakeTnr@users.noreply.github.com>
2025-04-10 15:35:07 -0400

    Retrieval frontend page
    frontend for retrival

--- src/App.tsx
+++ src/App.tsx
@@ -30,6 +30,7 @@ import NotificationDashboard from './NotificationDashboard'
 import NotificationDetail from './NotificationDetail'
 import { AuthProvider } from './components/AuthContext'
 import ItemPage from './ItemPage'
+import AdminRetrieve from './AdminRetrieve'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -58,6 +59,7 @@ function App() {
         <Route path="/view-messages" element={<ViewMessages />} />
         <Route path="/not-admin" element={<NotAdmin />} />
         <Route path="/admin-match" element={<AdminMatch />} />
+        <Route path="/admin-retrieve" element={<AdminRetrieve />} />
         <Route path="/notifications" element={<NotificationDashboard />} />
         <Route path="/notification-detail/:id" element={<NotificationDetail />} />
       </Routes>
