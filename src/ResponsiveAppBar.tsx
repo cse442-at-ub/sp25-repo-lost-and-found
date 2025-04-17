0f7193f0b8a6fe3e47c9ff1bb47a2c9d359b8610
Blake <80996688+BlakeTnr@users.noreply.github.com>
2025-04-17 09:48:35 -0400

    Update ResponsiveAppBar.tsx

--- src/ResponsiveAppBar.tsx
+++ src/ResponsiveAppBar.tsx
@@ -70,12 +70,12 @@ function ResponsiveAppBar() {
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          {/* <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} /> */}
           <Typography
             variant="h6"
             noWrap
             component="a"
-            href="/"
+            onClick={() => {navigate('/')}}
             sx={{
               mr: 2,
               display: { xs: 'none', md: 'flex' },
