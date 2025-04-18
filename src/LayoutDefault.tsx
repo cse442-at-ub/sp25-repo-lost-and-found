9d799029767c9ac95a45d116745d2665d08ef46a
Blake <80996688+BlakeTnr@users.noreply.github.com>
2025-04-18 11:02:39 -0400

    Fix pages and forms
    Fix some other pages and forms for holistic

--- src/LayoutDefault.tsx
+++ src/LayoutDefault.tsx
@@ -1,12 +1,12 @@
 import React, { Fragment } from 'react';
 import { Route } from 'react-router';
-import ResponsiveAppBar from './components/ResponsiveAppBar';
+import ResponsiveAppBarBad from './components/ResponsiveAppBar';
 import { Box } from '@mui/material';
 
 const LayoutDefault = (props: any) => (
   <>
     <Box sx={{minHeight: '100vh', display: 'flex', flexDirection: 'column'}}>
-      <ResponsiveAppBar />
+      <ResponsiveAppBarBad />
       {props.children}
     </Box>
   </>
